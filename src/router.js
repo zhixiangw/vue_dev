c4edc9bf251842c6e8657848582fa04ffc6a68d7
wangzhixiang <wangzhixiang@ehousechina.com>
2017-03-06 15:28:41 +0800

    路由实现按需加载

--- src/router.js
+++ src/router.js
@@ -1,20 +1,11 @@
-import Home from './componets/home/Home.vue'
-import Test1 from './componets/test1/Test1.vue'
-import Welcome from './componets/welcome/Welcome.vue'
-import Login from './componets/login/Login.vue'
-
-import HomeContainer from './common/home-container/PageContainer.vue'
-
 export default [
   { path: '/', redirect: '/login' },
-  { path: '/welcome', component: HomeContainer,
-    children: [{
-      path: '/', component: Welcome
-    }, {
-      path: '/test1', component: Test1
-    }, {
-      path: '/home', component: Home
-    }]
+  { path: '/welcome', component: resolve => require(['./common/home-container/PageContainer.vue'], resolve),
+    children: [
+      { path: '/', component: resolve => require(['./componets/welcome/Welcome.vue'], resolve) },
+      { path: '/test1', component: resolve => require(['./componets/test1/Test1.vue'], resolve) },
+      { path: '/home', component: resolve => require(['./componets/home/Home.vue'], resolve) }
+    ]
   },
-  { path: '/login', component: Login }
+  { path: '/login', component: resolve => require(['./componets/login/Login.vue'], resolve) }
 ]
